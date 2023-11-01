c88c9815d3c70d44c319b224c10b1f3fee113b35
rodrigo-devpass <123122131+rodrigo-devpass@users.noreply.github.com>
2023-11-01 14:08:51 -0300

    Update control_flow_challenge.rb

--- control_flow_challenge.rb
+++ control_flow_challenge.rb
@@ -1,6 +1,15 @@
 def grade(score)
-    # Add your solution here!
-    'N/A'
+    if score >= 90
+        'A'
+    elsif score >= 80
+        'B'
+    elsif score >= 70
+        'C'
+    elsif score >= 60
+        'D'
+    else
+        'F'
+    end
 end
 
 puts grade(95)  # Expected: 'A'
